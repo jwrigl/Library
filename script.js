b0339860662c3a1cd4df0c0a21f550ff00f1b86c
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-06-21 16:36:59 +0100

    Add addBook function, add comments

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 let library = [];
 
 
-function Book(title, author, isbn) {
+function Book(title, author, genre) {
+    //construct book object
     this.title = title;
     this.author = author;
-    this.isbn = isbn;
+    this.genre = genre;
     this.recall = function() {
-        console.log(this.title + " " + this.author + " " + this.isbn);
+        console.log(this.title + " " + this.author + " " + this.genre);
     }
 }
 
-const book1 = new Book("book1", "author1", "isbn1");
+const book1 = new Book("book1", "author1", "genre1");
 
 book1.recall();
 
-
+function addBook() {
+    //adds book to the library list
+}
 
 function buildLibrary() {
-
+ //Takes data from library array and builds HTML
 }
 

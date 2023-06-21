08b1f31c707fd3f5e52d694026f6bf33c8bcc220
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-06-21 19:20:20 +0100

    Add function to handle data entry from HTML inputs

--- script.js
+++ script.js
@@ -15,8 +15,30 @@ const book1 = new Book("book1", "author1", "genre1");
 
 book1.recall();
 
-function addBook() {
+function handleDataEntry() {
+    let titleInput = document.getElementById("bookTitle")
+    let authorInput = document.getElementById("bookAuthor")
+    let genreInput = document.getElementById("bookGenre")
+
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let genre = genreInput.value;
+
+    let book = new Book(title, author, genre);
+
+    titleInput.value, authorInput.value, genreInput.value = "";
+
+
+
+
+    addBook(book);
+}
+
+function addBook(book) {
     //adds book to the library list
+    library.push(book);
+
+    buildLibrary();
 }
 
 function buildLibrary() {
